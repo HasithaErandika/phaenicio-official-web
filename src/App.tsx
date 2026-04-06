c32a08928308b169e4d5d7a5b2fc3a924527f4dd
Hasitha Erandika <wickramasinghe.erandika@gmail.com>
2026-04-06 14:42:50 +0530

    refactor: import About component from dedicated file instead of OtherPages module

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import { About, Services, Contact } from './pages/OtherPages';
+import About from './pages/About';
+import { Services, Contact } from './pages/OtherPages';
 import ParticleBackground from './components/ParticleBackground';
 
 function App() {
